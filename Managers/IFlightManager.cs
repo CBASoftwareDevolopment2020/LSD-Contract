0ae6227eb16e35bce3146feb3c54114635616575
AdamSai <adamsaidane252@gmail.com>
2020-09-30 11:18:21 +0200

    Added method to show schedule between two airports to the IFlightManager interface based on the SsSD

--- Managers/IFlightManager.cs
+++ Managers/IFlightManager.cs
@@ -13,9 +13,17 @@ namespace LSD_Contract.Managers
         /// <summary>
         /// Get a list of flight details.
         /// </summary>
-        /// <param name="flights">The list of flights you wish to get details from</param>
+        /// <param name="flights">The list of flights you wish to get details from.</param>
         /// <returns>ICollection of FlightDetails</returns>
         ICollection<FlightDetail> GetFlightDetails(ICollection<FlightIdentifier> flights);
 
+        /// <summary>
+        /// Returns all flights between two airports on a given day.
+        /// </summary>
+        /// <param name="IATAcode1">The IATACode airport code of the first airport.</param>
+        /// <param name="IATAcode2">The IATACode airport code of the second airport.</param>
+        /// <param name="date">The date of the flights your wish to see.</param>
+        /// <returns>ICollection of FlightDetails</returns>
+        ICollection<FlightDetail> GetSchedule(string IATAcode1, string IATAcode2, DateTime date);
     }
 }
